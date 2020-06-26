85ba20a528154f782bba91db69bd2f34c26f9a5b
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-26 00:53:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,21 @@ def batch_badge_creator(names)
     end
   return badges
 end
+def assign_rooms(names)
+  assignments = []
+  room_num = 0
+  names.each do |e|
+    room_num++
+    assignments.push("Hello, "+ e +  "! You'll be assigned to room "+ room_num.to_s +  "!")
+  end
+  return assignments
+end
 
+peeps = ["Edsger", "Ada",
+"Charles", "Alan",
+"Grace", "Linus", "Matz"]
 
+rooms = [1,2,3,4,5,6,7]
 
+batch_badge_creator(peeps)
+assign_rooms(peeps)
